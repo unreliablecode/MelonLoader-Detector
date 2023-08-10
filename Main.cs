61525e38516b01b03fe200f1c2eea9e3dd8c884f
NULL Field <78525180+unreliablecode@users.noreply.github.com>
2023-08-10 15:20:21 +0700

    Exit Game When Modloader Detected

--- Main.cs
+++ Main.cs
@@ -13,18 +13,31 @@ public class ModLoaderDetector : MonoBehaviour
 
     private void Start()
     {
-        CheckModLoaders();
+        StartCoroutine(CheckForModLoaders());
     }
 
-    private void CheckModLoaders()
+    private System.Collections.IEnumerator CheckForModLoaders()
     {
-        bool melonLoaderLoaded = IsModuleLoaded(MelonLoaderModuleName);
-        bool harmonyLoaded = IsModuleLoaded(HarmonyModuleName);
-        bool bepInExCoreLoaded = IsModuleLoaded(BepInExCoreModuleName);
+        while (true)
+        {
+            bool melonLoaderLoaded = IsModuleLoaded(MelonLoaderModuleName);
+            bool harmonyLoaded = IsModuleLoaded(HarmonyModuleName);
+            bool bepInExCoreLoaded = IsModuleLoaded(BepInExCoreModuleName);
 
-        Debug.Log($"MelonLoader is{(melonLoaderLoaded ? "" : " not")} loaded.");
-        Debug.Log($"Harmony is{(harmonyLoaded ? "" : " not")} loaded.");
-        Debug.Log($"BepInEx.Core is{(bepInExCoreLoaded ? "" : " not")} loaded.");
+            Debug.Log($"MelonLoader is{(melonLoaderLoaded ? "" : " not")} loaded.");
+            Debug.Log($"Harmony is{(harmonyLoaded ? "" : " not")} loaded.");
+            Debug.Log($"BepInEx.Core is{(bepInExCoreLoaded ? "" : " not")} loaded.");
+
+            if (melonLoaderLoaded || harmonyLoaded || bepInExCoreLoaded)
+            {
+                Debug.Log("Exiting application...");
+                Application.Quit();
+                yield break;
+            }
+
+            // Adjust the delay here (in seconds) before the next check
+            yield return new WaitForSeconds(5.0f);
+        }
     }
 
     private bool IsModuleLoaded(string moduleName)
