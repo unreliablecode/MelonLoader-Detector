1a12f42fa6c29afe5666d71e30927b704a13f2c7
NULL Field <78525180+unreliablecode@users.noreply.github.com>
2023-08-10 15:28:17 +0700

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,17 +1,15 @@
 using System;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using System.IO;
 
 public class ModLoaderDetector : MonoBehaviour
 {
-    const string MelonLoaderModuleName = "MelonLoader.dll";
-    const string HarmonyModuleName = "Harmony0.dll";
-    const string BepInExCoreModuleName = "BepInEx.Core.dll";
+    private string[] moduleNames = { "MelonLoader.dll", "Harmony0.dll", "BepInEx.Core.dll" };
+    private string[] folderNames = { "MelonLoader", "BepInEx" };
 
     [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
     public static extern IntPtr GetModuleHandle(string lpModuleName);
-
-    private bool hasDetectedModLoader = false;
 
     private void Start()
     {
@@ -19,22 +17,35 @@ public class ModLoaderDetector : MonoBehaviour
         CheckForModLoaders();
     }
 
-    private System.Collections.IEnumerator CheckForModLoaders()
+    private void CheckForModLoaders()
     {
-        bool melonLoaderLoaded = IsModuleLoaded(MelonLoaderModuleName);
-        bool harmonyLoaded = IsModuleLoaded(HarmonyModuleName);
-        bool bepInExCoreLoaded = IsModuleLoaded(BepInExCoreModuleName);
+        bool modLoaded = false;
+        bool folderPresent = false;
 
-        Debug.Log($"MelonLoader is{(melonLoaderLoaded ? "" : " not")} loaded.");
-        Debug.Log($"Harmony is{(harmonyLoaded ? "" : " not")} loaded.");
-        Debug.Log($"BepInEx.Core is{(bepInExCoreLoaded ? "" : " not")} loaded.");
+        foreach (string moduleName in moduleNames)
+        {
+            if (IsModuleLoaded(moduleName))
+            {
+                Debug.Log($"{moduleName} is loaded.");
+                modLoaded = true;
+                break;
+            }
+        }
 
-        // If any mod loader is detected, exit the application
-        if (melonLoaderLoaded || harmonyLoaded || bepInExCoreLoaded)
+        foreach (string folderName in folderNames)
+        {
+            if (Directory.Exists(folderName))
+            {
+                Debug.Log($"{folderName} folder is present.");
+                folderPresent = true;
+                break;
+            }
+        }
+
+        if (modLoaded || folderPresent)
         {
             Debug.Log("Exiting application...");
             Application.Quit();
-            yield break;
         }
     }
 
@@ -46,9 +57,7 @@ public class ModLoaderDetector : MonoBehaviour
 
     private void Update()
     {
-        // Start the coroutine to check for mod loaders
-        StartCoroutine(CheckForModLoaders());
-
-        // Update logic here if needed
+        // Check for mod loaders and folders
+        CheckForModLoaders();
     }
 }
